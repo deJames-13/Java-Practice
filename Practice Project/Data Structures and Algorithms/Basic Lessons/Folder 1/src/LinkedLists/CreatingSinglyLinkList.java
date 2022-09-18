50a9bc729620606fcfb940a1fdc20b7411aa1ee4
deJames-13 <de.james013@gmail.com>
2022-09-18 16:48:53 +0800

    091822

--- Practice Project/Data Structures and Algorithms/Basic Lessons/Folder 1/src/LinkedLists/CreatingSinglyLinkList.java
+++
@@ -1,7 +0,0 @@
-package LinkedLists;
-
-public class CreatingSinglyLinkList {
-    public static void main(String[] args) {
-
-    }
-}
--- Practice Project/Data Structures and Algorithms/Basic Lessons/Folder 1/src/Scratch.java
+++ Practice Project/Data Structures and Algorithms/Basic Lessons/Folder 1/src/Scratch.java
@@ -1,10 +1,34 @@
-/**
- * Scratch
- */
 public class Scratch {
-
     public static void main(String[] args) {
-        int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        System.out.println(myArray[0]);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if ((i == 0 || j == 0) || (i == 9 || j == 9) || (i == j)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if ((i == 0 || j == 9) || (i == j)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if ((i == 9 || j == 0) || (i == j)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
